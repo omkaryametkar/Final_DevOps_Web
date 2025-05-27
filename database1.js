fd7e75d3f5cd58b5e6d7c739ffa66af150d0509e
Mudabbir Naragaddi <121452070+naragaddimudabbir@users.noreply.github.com>
2025-05-27 16:10:31 +0530

    Create database1.js

---
+++ database1.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql');
+print("this is database code")
+// MySQL connection configuration
+const databaseConnection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root', // Replace with your MySQL username
+  password: 'root@123', // Replace with your MySQL password
+});
+
+// Connect to MySQL server
+databaseConnection.connect((err) => {
+  if (err) {
+    console.error('Database connection failed:', err.stack);
+    return;
+  }
+  console.log('Connected to MySQL server');
+
+  // Create the database
+  const createDatabaseQuery = 'CREATE DATABASE Movie';
+  databaseConnection.query(createDatabaseQuery, (err, result) => {
+    if (err) throw err;
+    console.log('Database created successfully!');
+    databaseConnection.end(); // Close the connection
+  });
+});
